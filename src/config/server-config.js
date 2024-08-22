74f716110e4fb6414b5af930781de2b31f97b345
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-08-23 03:37:49 +0530

    Fact: Added proxy middleware for search service, also also improve the logs using morgan and added rate limiting

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    FLIGHT_SEARCH_REQUEST_URL: process.env.FLIGHT_SEARCH_REQUEST_URL
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/server-config');
-
+const {PORT, FLIGHT_SEARCH_REQUEST_URL} = require('./config/server-config');
+const morgan = require('morgan');
+const { rateLimit } = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 async function setupAndStartServer(){
 
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use(morgan('combined'));
+
+    const limiter = {
+        windowMs: 5 * 60 * 1000,
+        max: 5
+    }
+
+    app.use(rateLimit(limiter));
+
+    app.use('/searchService', createProxyMiddleware({target: FLIGHT_SEARCH_REQUEST_URL, changeOrigin: true}))
+    
+    app.get('/hello', (req, res) => {
+        return res.status(200).json({
+            message: "Hello user"
+        })
+    })
 
     app.listen(PORT, () => {
         console.log(`Server is running at PORT ${PORT}`);
